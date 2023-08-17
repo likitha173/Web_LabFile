a3482d90b889f24845ad07d8fc9b043fc997c032
Likitha Khatri <likithakhatri5774@gmail.com>
2023-08-18 01:04:34 +0530

    web

--- exp19_16.php
+++ exp19_16.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <!-- Fig. 19.16: database.php -->
-<!-- Querying a database and displaying the results. -->
+<!-- Querying a database and displaying the results -->
 <html>
    <head>
       <meta charset = "utf-8">
